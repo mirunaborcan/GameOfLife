53caa10c9105129675c5bd02a206d2c59a01df68
poppaula <poppaula0106@gmail.com>
2022-12-06 09:13:06 +0200

    'kafka'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hello");
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World");
     }
 }
